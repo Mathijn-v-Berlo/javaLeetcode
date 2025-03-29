b4897eb43cd2dddad2856235c6476f77aa7739e2
Mathijn van Berlo <158485373+Mathijn-v-Berlo@users.noreply.github.com>
2025-03-29 16:23:09 +0100

    TopKFrequentElements (#2)
    Co-authored-by: Mathijn van Berlo <mathijn.van.berlo@keronic.com>

--- src/main/java/com/mathijnvanberlo/leetcode/arrays_and_hashing/TopKFrequentElements.java
+++ src/main/java/com/mathijnvanberlo/leetcode/arrays_and_hashing/TopKFrequentElements.java
@@ -1,7 +1,59 @@
 package com.mathijnvanberlo.leetcode.arrays_and_hashing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class TopKFrequentElements {
+    // Given an integer array nums and an integer k,
+    // return the k most frequent elements.
+    // You may return the answer in any order.
+
     public int[] topKFrequent(int[] nums, int k) {
-	return new int[]{1};
+	HashMap<Integer, int[]> freqMap = new HashMap<>();
+
+	for (int num: nums) {
+	    int[] freq = freqMap.getOrDefault(num, new int[]{num, 0});
+	    freq[1]++;
+	    freqMap.put(num, freq);
+	}
+
+	int[][] freqArrays = freqMap.values().toArray(new int[][]{});
+	Arrays.sort(freqArrays, (a, b) -> Integer.compare(b[1], a[1]));
+
+	int[] result = new int[k];
+	for (int i = 0; i < k; i++) {
+	    result[i] = freqArrays[i][0];
+	}
+	return result;
+    }
+
+    public int[] topKFrequentBucketSort(int[] nums, int k) {
+	HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+	for (int num : nums) {
+	    int freq = freqMap.getOrDefault(num, 0);
+	    freq++;
+	    freqMap.put(num, freq);
+	}
+
+	ArrayList<Integer>[] bucketSort = new ArrayList[nums.length];
+	for (int i = 0; i < bucketSort.length; i++) bucketSort[i] = new ArrayList<>();
+
+	for (int key : freqMap.keySet()) {
+	    bucketSort[freqMap.get(key) - 1].add(key);
+	}
+
+	int[] result = new int[k];
+	int found = 0;
+	for (int i = nums.length - 1; i >= 0; i--) {
+	    for (int num : bucketSort[i]) {
+		result[found] = num;
+		found++;
+		if (found == k) return result;
+	    }
+	}
+	return result;
     }
 }
--- src/test/java/com/mathijnvanberlo/leetcode/arrays_and_hashing/TopKFrequentElementsTest.java
+++ src/test/java/com/mathijnvanberlo/leetcode/arrays_and_hashing/TopKFrequentElementsTest.java
@@ -1,15 +1,61 @@
 package com.mathijnvanberlo.leetcode.arrays_and_hashing;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
 class TopKFrequentElementsTest {
+    // Given an integer array nums and an integer k,
+    // return the k most frequent elements.
+    // You may return the answer in any order.
 
     @Test
     void topKFrequentElementsTest() {
-        TopKFrequentElements topKFrequentElements = new TopKFrequentElements();
-        int[] test = topKFrequentElements.topKFrequent(new int[] { 1 }, 1);
-        assertEquals(1, 0, "TODO");
+	
+	int[] input = new int[] { 1 };
+	int[] result = sortedResults(input, 1);
+	assertEquals(1, result[0], "We expect result to be {1}");
+	result = sortedResultsBucketSort(input, 1);
+	assertEquals(1, result[0], "We expect result to be {1}");
+	
+	input = new int[] { 1, 1, 1, 2, 2, 3 };
+	result = sortedResults(input, 2);
+	assertEquals(1, result[0], "We expect result to be {1, 2}");
+	assertEquals(2, result[1], "We expect result to be {1, 2}");
+	result = sortedResultsBucketSort(input, 2);
+	assertEquals(1, result[0], "We expect result to be {1, 2}");
+	assertEquals(2, result[1], "We expect result to be {1, 2}");
+
+	input = new int[] { 1, 2, 2, 3, 3 };
+	result = sortedResults(input, 2);
+	assertEquals(2, result[0], "We expect result to be {2, 3}");
+	assertEquals(3, result[1], "We expect result to be {2, 3}");
+	result = sortedResultsBucketSort(input, 2);
+	assertEquals(2, result[0], "We expect result to be {2, 3}");
+	assertEquals(3, result[1], "We expect result to be {2, 3}");
+    }
+
+    private int[] sortedResults(int[] input, int k) {
+	int[] result = result(input, k);
+	Arrays.sort(result);
+	return result;
+    }
+
+    private int[] sortedResultsBucketSort(int[] input, int k) {
+	int[] result = resultBucketSort(input, k);
+	Arrays.sort(result);
+	return result;
+    }
+
+    private int[] result(int[] input, int k) {
+	TopKFrequentElements topKFrequentElements = new TopKFrequentElements();
+	return topKFrequentElements.topKFrequent(input, k);
+    }
+
+    private int[] resultBucketSort(int[] input, int k) {
+	TopKFrequentElements topKFrequentElements = new TopKFrequentElements();
+	return topKFrequentElements.topKFrequentBucketSort(input, k);
     }
 }
